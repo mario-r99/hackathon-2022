9c0de60feb2c687f8ddd068c5a0a3fc7f6467412
fl3on <f.leon.marquez95@gmail.com>
2022-07-07 20:58:40 +0200

    feat: server ready & connect with ui backend

--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ from flask import request
 import json
 from blockchainer import *
 from utils import *
+from ..visualization.push import push_data
 
 app = flask.Flask(__name__)
 
@@ -12,19 +13,29 @@ app = flask.Flask(__name__)
 def handle_call():
     return "Successfully Connected"
 
+# this must be used for further improvement
 @app.route('/getlast', methods=['GET'])
 def get_last_block():
     config = read_config_file()
-    data = get_last_values_from_blockchain(config)
+    station, timestamp = get_last_values_from_blockchain(config)
+    data = "{\"id\":\"" + str(station)+"\",\"timestamp\":\"" + str(timestamp) + "\"}"
     return data
 
 #the post method. when we call this with a string containing a name, it will return the name with the text "I got your name"
 @app.route('/scanner', methods=['POST'])
 def extract_data():
     global data
+
+    ## Writing data
     data = request.get_json()
     config = read_config_file()
     persist_in_blockchain(config, data["id"], data["timestamp"])
+
+    ## Reading data
+    station, timestamp = get_last_values_from_blockchain(config)
+    data = "{\"id\":\"" + str(station)+"\",\"timestamp\":\"" + str(timestamp) + "\"}"
+    push_data(data)
+
     return "OK"
 
 #this commands the script to run in the given port
