e8be4f29ce135f372331e146aa1b14e8bed36557
mario-r99 <mario.srx99@gmail.com>
2022-07-07 22:27:31 +0200

    Add location reading

--- scanner/app/src/main/java/com/hackathon2022/scanner/ThirdFragment.kt
+++ scanner/app/src/main/java/com/hackathon2022/scanner/ThirdFragment.kt
@@ -1,11 +1,18 @@
 package com.hackathon2022.scanner
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.hackathon2022.scanner.databinding.FragmentThirdBinding
 import org.json.JSONObject
 import java.io.BufferedReader
@@ -25,6 +32,9 @@ class ThirdFragment : Fragment() {
 
     private var _binding: FragmentThirdBinding? = null
     private val serverUrl = "http://172.20.36.211:5000/scanner"
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var latitude = 48.748654568574594
+    private var longitude = 9.108372534029133
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -42,11 +52,41 @@ class ThirdFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context!!)
+
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    getNetworkLocation()
+                } else {
+                    Toast.makeText(context, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        when {
+            ContextCompat.checkSelfPermission(
+                activity!!,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                getNetworkLocation()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                Toast.makeText(context, "This app needs location access to transmit the machine location", Toast.LENGTH_LONG).show()
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+
         val scannedId = arguments?.getString("scannedID")
         binding.qrValue.text = scannedId
         binding.serverAddress.setText(serverUrl)
 
         binding.buttonFirst.setOnClickListener {
+            getNetworkLocation()
             val jsonData = JSONObject()
             jsonData.put("id", scannedId)
             val timestamp = DateTimeFormatter
@@ -54,6 +94,7 @@ class ThirdFragment : Fragment() {
                 .withZone(ZoneId.of("Europe/Berlin"))
                 .format(Instant.now())
             jsonData.put("timestamp", timestamp)
+            jsonData.put("location", "${latitude};${longitude}")
             println(jsonData)
 
             Thread {
@@ -62,9 +103,9 @@ class ThirdFragment : Fragment() {
                     requestMethod = "POST"
                     setRequestProperty("Content-Type", "application/json")
 
-                    val wr = OutputStreamWriter(outputStream);
-                    wr.write(jsonData.toString());
-                    wr.flush();
+                    val wr = OutputStreamWriter(outputStream)
+                    wr.write(jsonData.toString())
+                    wr.flush()
 
                     println("URL : $url")
                     println("Response Code : $responseCode")
@@ -89,5 +130,15 @@ class ThirdFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun getNetworkLocation() {
+        fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+            if (location!=null) {
+                latitude = location.latitude
+                longitude = location.longitude
+            }
+            else println("LOCATION IS NULL")
+        }
     }
 }
